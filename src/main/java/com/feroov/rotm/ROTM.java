c9d553556b6cf984dcaba809bd20bbd231d97c47
Feroov <feroovt@gmail.com>
2023-04-18 23:42:46 +0200

    Horsiper spawn egg

--- src/main/java/com/feroov/rotm/ROTM.java
+++ src/main/java/com/feroov/rotm/ROTM.java
@@ -51,6 +51,7 @@ public class ROTM
             event.accept(ItemsROTM.GUNSWINE_SPAWN_EGG);
             event.accept(ItemsROTM.COWPG_SPAWN_EGG);
             event.accept(ItemsROTM.STABBIT_SPAWN_EGG);
+            event.accept(ItemsROTM.HORSIPER_SPAWN_EGG);
         }
     }
 
--- src/main/java/com/feroov/rotm/item/ItemsROTM.java
+++ src/main/java/com/feroov/rotm/item/ItemsROTM.java
@@ -42,6 +42,9 @@ public class ItemsROTM
     public static final RegistryObject<Item> STABBIT_SPAWN_EGG = ITEMS.register("stabbit_spawn_egg",
             () -> new ForgeSpawnEggItem(EntitiesROTM.STABBIT, 0xFFFFFF, 0xFFFFFF, new Item.Properties()));
 
+    public static final RegistryObject<Item> HORSIPER_SPAWN_EGG = ITEMS.register("horsiper_spawn_egg",
+            () -> new ForgeSpawnEggItem(EntitiesROTM.HORSIPER, 0xFFFFFF, 0xFFFFFF, new Item.Properties()));
+
 
     public static void register(IEventBus eventBus)
     {
