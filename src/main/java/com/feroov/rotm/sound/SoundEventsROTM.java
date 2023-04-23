c42db203646008a7ce992c99d3653ec759119c8c
Feroov <feroovt@gmail.com>
2023-04-24 01:07:12 +0200

    Refactor tank -> tank_shot

--- src/main/java/com/feroov/rotm/sound/SoundEventsROTM.java
+++ src/main/java/com/feroov/rotm/sound/SoundEventsROTM.java
@@ -12,19 +12,14 @@ public class SoundEventsROTM
 {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, ROTM.MOD_ID);
 
-    // Gunswine
     public static final RegistryObject<SoundEvent> GUNSWINE_DEATH = SOUND_EVENTS.register("gunswine_death", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "gunswine_death")));
-
-
     public static final RegistryObject<SoundEvent> AK47 = SOUND_EVENTS.register("ak47", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "ak47")));
     public static final RegistryObject<SoundEvent> ROCKET = SOUND_EVENTS.register("rocket", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "rocket")));
     public static final RegistryObject<SoundEvent> FIFTY_CAL = SOUND_EVENTS.register("50cal", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "50cal")));
     public static final RegistryObject<SoundEvent> DEAGLE = SOUND_EVENTS.register("deagle", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "deagle")));
-    public static final RegistryObject<SoundEvent> TANK = SOUND_EVENTS.register("tank", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "tank")));
+    public static final RegistryObject<SoundEvent> TANK_SHOT = SOUND_EVENTS.register("tank", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "tank")));
     public static final RegistryObject<SoundEvent> TANK_IDLE = SOUND_EVENTS.register("tank_idle", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "tank_idle")));
     public static final RegistryObject<SoundEvent> MECHAMOO_SHOT = SOUND_EVENTS.register("mechamoo_shot", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(ROTM.MOD_ID, "mechamoo_shot")));
-
-
 
     public static void register(IEventBus eventBus) { SOUND_EVENTS.register(eventBus); }
 }
