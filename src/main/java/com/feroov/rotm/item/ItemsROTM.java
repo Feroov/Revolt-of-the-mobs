c011eb731970acd882d8ca4999df58d863bb292a
zyenerr <131720925+zyenerr@users.noreply.github.com>
2023-04-25 00:56:24 +0200

    Update ItemsROTM.java

--- src/main/java/com/feroov/rotm/item/ItemsROTM.java
+++ src/main/java/com/feroov/rotm/item/ItemsROTM.java
@@ -28,7 +28,7 @@ public class ItemsROTM
         public void appendHoverText(ItemStack stack, Level worldIn, List<Component> tooltip, TooltipFlag flagIn)
         {
             super.appendHoverText(stack, worldIn, tooltip, flagIn);
-            tooltip.add(Component.translatable("Banish every being off the screen")
+            tooltip.add(Component.translatable("Verbannen Sie jede Kreatur vom Bildschirm")
                     .withStyle(ChatFormatting.RED));
         }
     });
