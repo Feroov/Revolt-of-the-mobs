0f1bc1e471f8435200e6416801733841d7926f24
Feroov <feroovt@gmail.com>
2023-05-03 21:28:04 +0200

    AI Change for Gunswine

--- src/main/java/com/feroov/rotm/entity/hostile/Gunswine.java
+++ src/main/java/com/feroov/rotm/entity/hostile/Gunswine.java
@@ -16,6 +16,7 @@ import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.*;
 import net.minecraft.world.entity.monster.*;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ProjectileWeaponItem;
@@ -57,6 +58,7 @@ public class Gunswine extends Monster implements GeoEntity
         this.goalSelector.addGoal(1, new OpenDoorGoal(this,true));
         this.targetSelector.addGoal(2, new GunswineAttackGoal(this, 0.0D, true, 3));
         this.goalSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        this.goalSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, true));
         this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Mob.class, 5, false, false, (p_28879_) -> {
             return p_28879_ instanceof Enemy && !(p_28879_ instanceof Gunswine);
         }));
