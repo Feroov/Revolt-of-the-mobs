5cdff848ab069e1bd1534963125975763be0c086
Feroov <feroovt@gmail.com>
2023-05-03 23:25:26 +0200

    AI change for Cowpg

--- src/main/java/com/feroov/rotm/entity/hostile/Cowpg.java
+++ src/main/java/com/feroov/rotm/entity/hostile/Cowpg.java
@@ -66,6 +66,7 @@ public class Cowpg extends Monster implements GeoEntity
             return p_28879_ instanceof Enemy && !(p_28879_ instanceof Cowpg) && !(p_28879_ instanceof MonsterTank) && !(p_28879_ instanceof Mechamoo);
         }));
         this.goalSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, Animal.class, true));
+        this.goalSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, FlyingMob.class, true));
         this.goalSelector.addGoal(5, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
         this.goalSelector.addGoal(4, new CowpgRangedAttackGoal(this, 0.1D, 40.0D, 73.0F, 0));
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 0.4D));
